f2ed01e9dcd7a9d4afc4fb92a36a54e56726af61
electroem3000 <em_vera@mail.ru>
2024-11-27 16:48:38 +0300

    Updated lab5

--- BeautyShop/Program.cs
+++ BeautyShop/Program.cs
@@ -1,16 +1,18 @@
 using BeautyShop.DI;
-using BeautyShop.Service.IoC;
 using BeautyShop.Settings;
-using Service.loC;
 
 var configuration = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", optional: false)
     .Build();
+
 var settings = BeautyShopSettingsReader.Read(configuration);
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 ApplicationConfigurator.ConfigureServices(builder, settings);
 
 var app = builder.Build();
+
+ApplicationConfigurator.ConfigureApplication(app);
+
+app.Run();
